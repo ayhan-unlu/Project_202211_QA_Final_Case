848cccc68aa23de3e5ce13fb82a31e20da0f1497
Ayhan Unlu <ayhan-unlu@hotmail.com>
2022-12-01 10:15:03 +0300

    assertions all functional

--- ebebek_qa_finalcase/src/test/java/patika/practicum/SingleClassFinalCaseQATest.java
+++ ebebek_qa_finalcase/src/test/java/patika/practicum/SingleClassFinalCaseQATest.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,33 +21,36 @@ public class SingleClassFinalCaseQATest {
 
   public static void main(String[] args) throws Exception {
 
+    //Congigurations
+
     String baseUrl = "https://www.e-bebek.com";
     System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\SeleniumProject\\src\\drivers\\chromedriver.exe");
     // Mentioning the driver we use and indicating its location
 
-    // webdriver error solved with adding jar file source
+    // webdriver error occured then solved with adding jar file
     WebDriver driver = new ChromeDriver();// for driver initilisation or creating a driver object in java
     JavascriptExecutor js;
     js = (JavascriptExecutor) driver;
+
+    //Task 1. https://www.e-bebek.com/ website should be opened.
+
     driver.get(baseUrl);// to indicate where you want to go https:// must be included
 
     // source : https://www.youtube.com/watch?v=sxmwGdZbV-Y&t=3s
 
     // System.out.println(driver.getTitle());//for printing website title
 
-    driver.manage().window().maximize();// maximize the window
-    // Thread.sleep(1000);// waits for 1 second to go to next line of code
+    driver.manage().window().maximize();// maximize the window    
+    Thread.sleep(2000);// waits for 2 second to go to next line of code
 
+
+    //ebebek logo is controlled in the main page 
     WebElement mainPageLogoElement = driver.findElement(By.xpath("//img[@alt='Ebebek Logo Banner']"));
     Assert.assertTrue(mainPageLogoElement.isDisplayed());
 
-    // driver.findElement(By.id("txtSearchBox")).sendKeys("kaşık maması",
-    // Keys.ENTER);
-
-    // Thread.sleep(1000);// waits for 1 second to go to next line of code
-
-    // driver.get("https://www.e-bebek.com/");
     // Thread.sleep(2000);// waits for 2 second to go to next line of code
+
+    //Task 2. Enter "kaşık mama"-"spoon baby food" in the search bar and the last product should be clicked from the results of the search, it should be verified that the opened product and the product clicked in the search result are the same.
 
     driver.findElement(By.id("txtSearchBox")).sendKeys("kaşık maması");
     Thread.sleep(2000);// waits for 2 second to go to next line of code
@@ -65,46 +66,47 @@ public class SingleClassFinalCaseQATest {
     js.executeScript("window.scrollTo(0,5530)");
     Thread.sleep(2000);// waits for 2 second to go to next line of code
 
-    String nameOfLastItemOnTheSearchPage = driver
-        .findElement(By.xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).getAccessibleName();
+   /* 
+   String nameOfLastItemOnTheSearchPage = driver.findElement(By.xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).getAccessibleName();
+   System.out.println(nameOfLastItemOnTheSearchPage); 
+   */
 
     driver.findElement(By.xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).click();
 
+
     Thread.sleep(2000);// waits for 2 second to go to next line of code
-
-    // System.out.println("ürün sayfasında bulunan isim");
-
-    // js.executeScript("window.scrollTo(0,2875)");
 
     String expectedUrl = "https://www.e-bebek.com/oioi-mama-kasigi-2li-aqua-green--mellow-yellow-p-oioi-1060016";
     String actualUrl = driver.getCurrentUrl();
+ 
     Assert.assertEquals(expectedUrl, actualUrl); // Thread.sleep(2000);//waits for 2 second to go to next line of code
-    
-    
-    //https://www.e-bebek.com/oioi-mama-kasigi-2li-aqua-green--mellow-yellow-p-oioi-1060016
 
     Thread.sleep(2000);// waits for 2 second to go to next line of code
 
+    //Task 3. The product should be added to the cart.
+    
     driver.findElement(By.id("addToCartBtn")).click();
-
     Thread.sleep(2000);// waits for 2 second to go to next line of code
 
-  //  WebElement visibleShowCartButton = driver.findElement(By.xpath("[text()=' Ürün Güncellendi ']"));
- //   Assert.assertTrue(visibleShowCartButton.isDisplayed());
+    WebElement showCartButton = driver.findElement(By.xpath("//a[@id='btnShowCart']"));
+    Assert.assertTrue(showCartButton.isDisplayed());
+
+    //Task 4. Click the view cart button from the right-handed mode-window.
 
     driver.findElement(By.xpath("//a[@id='btnShowCart']")).click();
-
-    Thread.sleep(2000);// waits for 1 second to go to next line of code
-    // js.executeScript("window.scrollTo(0,1377)");
+    Thread.sleep(2000);// waits for 2 second to go to next line of code
 
     expectedUrl = "https://www.e-bebek.com/cart";
     actualUrl = driver.getCurrentUrl();
-    Assert.assertEquals(expectedUrl, actualUrl); // Thread.sleep(2000);//waits for 2 second to go to next line of code
-    driver.findElement(By.id("btnGoToShippingAddress")).click();
+    Assert.assertEquals(expectedUrl, actualUrl); 
+    Thread.sleep(2000);//waits for 2 second to go to next line of code
+    
+    //Task 5. Click the Complete Shopping button.
 
+    driver.findElement(By.id("btnGoToShippingAddress")).click();
     Thread.sleep(2000);// waits for 2 second to go to next line of code
 
-    // System.out.println(driver.getTitle());// for printing website title
+    //Task 6. It should be verified that the member login screen is on.
 
     expectedUrl = "https://www.e-bebek.com/login?forced=true";
     actualUrl = driver.getCurrentUrl();
