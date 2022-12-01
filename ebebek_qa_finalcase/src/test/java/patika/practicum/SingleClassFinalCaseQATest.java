87f715c2c02b7e38acad29fbff84ff100709ddac
Ayhan Unlu <ayhan-unlu@hotmail.com>
2022-12-01 14:27:29 +0300

    JUnit test is executed on a single class file

--- ebebek_qa_finalcase/src/test/java/patika/practicum/SingleClassFinalCaseQATest.java
+++ ebebek_qa_finalcase/src/test/java/patika/practicum/SingleClassFinalCaseQATest.java
@@ -1,13 +1,16 @@
 package patika.practicum;
 
 import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+//import java.time.Duration;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,9 +24,10 @@ import org.apache.logging.log4j.Logger;
 public class SingleClassFinalCaseQATest {
   static Logger logger = LogManager.getLogger(SingleClassFinalCaseQATest.class);
 
-  public static void main(String[] args) throws Exception {
+  @Test
+  public void test() throws InterruptedException {
 
-    //Congigurations
+    // Configurations
 
     String baseUrl = "https://www.e-bebek.com";
     System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\SeleniumProject\\src\\drivers\\chromedriver.exe");
@@ -34,7 +38,7 @@ public class SingleClassFinalCaseQATest {
     JavascriptExecutor js;
     js = (JavascriptExecutor) driver;
 
-    //Task 1. https://www.e-bebek.com/ website should be opened.
+    // Task 1. https://www.e-bebek.com/ website should be opened.
 
     driver.get(baseUrl);// to indicate where you want to go https:// must be included
 
@@ -42,84 +46,96 @@ public class SingleClassFinalCaseQATest {
 
     // System.out.println(driver.getTitle());//for printing website title
 
-    driver.manage().window().maximize();// maximize the window    
-  //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));// waits until 15 seconds maximum if required. But it wont be used in this project
-     waitForTwoSeconds();// waits for 2 second to go to next line of code
+    driver.manage().window().maximize();// maximize the window
+    // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));// waits
+    // until 15 seconds maximum if required. But it wont be used in this project
 
-    //ebebek logo is controlled in the main page 
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
+
+    // ebebek logo is controlled in the main page
     WebElement mainPageLogoElement = driver.findElement(By.xpath("//img[@alt='Ebebek Logo Banner']"));
     Assert.assertTrue(mainPageLogoElement.isDisplayed());
 
-
-    //Task 2. Enter "kaşık mama"-"spoon baby food" in the search bar and the last product should be clicked from the results of the search, it should be verified that the opened product and the product clicked in the search result are the same.
+    // Task 2. Enter "kaşık mama"-"spoon baby food" in the search bar and the last
+    // product should be clicked from the results of the search, it should be
+    // verified that the opened product and the product clicked in the search result
+    // are the same.
 
     driver.findElement(By.id("txtSearchBox")).sendKeys("kaşık maması");
-    waitForTwoSeconds();// waits for 2 second to go to next line of code
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
     driver.findElement(By.id("txtSearchBox")).sendKeys(Keys.ENTER);
-    waitForTwoSeconds();// waits for 2 second to go to next line of code
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
 
     js.executeScript("window.scrollTo(0,1163)");
-    waitForTwoSeconds();// used extra for better scrolling down 
+    TimeUnit.SECONDS.sleep(2);
+    TimeUnit.SECONDS.sleep(2);
+    // used extra for better scrolling down
     js.executeScript("window.scrollTo(0,2594)");
-    waitForTwoSeconds();
-    js.executeScript("window.scrollTo(0,4062)");
-    waitForTwoSeconds();
-    js.executeScript("window.scrollTo(0,5530)");
-    waitForTwoSeconds();
+    TimeUnit.SECONDS.sleep(2);
 
-   /* 
-   String nameOfLastItemOnTheSearchPage = driver.findElement(By.xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).getAccessibleName();
-   System.out.println(nameOfLastItemOnTheSearchPage); 
-   */
+    js.executeScript("window.scrollTo(0,4062)");
+    // TimeUnit.SECONDS.sleep(2);
+
+    js.executeScript("window.scrollTo(0,5530)");
+    TimeUnit.SECONDS.sleep(2);
+
+    /*
+     * String nameOfLastItemOnTheSearchPage = driver.findElement(By.
+     * xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).
+     * getAccessibleName();
+     * System.out.println(nameOfLastItemOnTheSearchPage);
+     */
 
     driver.findElement(By.xpath("//img[@alt='OiOi Mama Kaşığı 2’li Aqua Green- Mellow'] ")).click();
 
-
-    waitForTwoSeconds();
+    TimeUnit.SECONDS.sleep(2);
 
     String expectedUrl = "https://www.e-bebek.com/oioi-mama-kasigi-2li-aqua-green--mellow-yellow-p-oioi-1060016";
     String actualUrl = driver.getCurrentUrl();
- 
+
     Assert.assertEquals(expectedUrl, actualUrl);
 
-    waitForTwoSeconds();
+    TimeUnit.SECONDS.sleep(2);
 
-    //Task 3. The product should be added to the cart.
-    
+    // Task 3. The product should be added to the cart.
+
     driver.findElement(By.id("addToCartBtn")).click();
-    waitForTwoSeconds();
+    TimeUnit.SECONDS.sleep(2);
 
     WebElement showCartButton = driver.findElement(By.xpath("//a[@id='btnShowCart']"));
     Assert.assertTrue(showCartButton.isDisplayed());
 
-    //Task 4. Click the view cart button from the right-handed mode-window.
+    // Task 4. Click the view cart button from the right-handed mode-window.
 
     driver.findElement(By.xpath("//a[@id='btnShowCart']")).click();
-    waitForTwoSeconds();// waits for 2 second to go to next line of code
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
 
     expectedUrl = "https://www.e-bebek.com/cart";
     actualUrl = driver.getCurrentUrl();
-    Assert.assertEquals(expectedUrl, actualUrl); 
-    waitForTwoSeconds();//waits for 2 second to go to next line of code
-    
-    //Task 5. Click the Complete Shopping button.
+    Assert.assertEquals(expectedUrl, actualUrl);
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
+
+    // Task 5. Click the Complete Shopping button.
 
     driver.findElement(By.id("btnGoToShippingAddress")).click();
-    waitForTwoSeconds();// waits for 2 second to go to next line of code
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
 
-    //Task 6. It should be verified that the member login screen is on.
+    // Task 6. It should be verified that the member login screen is on.
 
     expectedUrl = "https://www.e-bebek.com/login?forced=true";
     actualUrl = driver.getCurrentUrl();
     Assert.assertEquals(expectedUrl, actualUrl);
 
-    waitForTwoSeconds();// waits for 2 second to go to next line of code
+    TimeUnit.SECONDS.sleep(2);
+    // waits for 2 second to go to next line of code
 
     driver.quit();// for leaving the website
 
-  }
-
-  private static void waitForTwoSeconds() throws InterruptedException {
-    Thread.sleep(2000);
   }
 }
